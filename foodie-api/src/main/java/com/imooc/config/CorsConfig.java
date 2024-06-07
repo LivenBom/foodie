a72f5c6a94988b150546ac06d23ebba2db7deafa
LivenChief <liven_zhan@chiefgroup.com.hk>
2024-06-07 13:52:01 +0800

    跨域访问设置

---
+++ foodie-api/src/main/java/com/imooc/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package com.imooc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    public CorsConfig() {
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        // 1. 添加 CORS 配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        // 设置允许跨域访问的域名，这里是设置Web前端的域名
+        config.addAllowedOrigin("http://localhost:8080");
+
+        // 设置是否发送cookie信息
+        config.setAllowCredentials(true);
+
+        // 设置允许请求的方式
+        config.addAllowedMethod("*");
+
+        // 设置允许的header
+        config.addAllowedHeader("*");
+
+        // 2. 为 URL 添加映射路径
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        corsSource.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(corsSource);
+    }
+}
