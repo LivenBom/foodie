02f979b8d3dd9281884845ebe078ecbbb5467dda
LivenChief <liven_zhan@chiefgroup.com.hk>
2024-06-17 23:12:36 +0800

    删除购物车商品

--- foodie-api/src/main/java/com/imooc/controller/ShopcarController.java
+++ foodie-api/src/main/java/com/imooc/controller/ShopcarController.java
@@ -30,4 +30,18 @@ public class ShopcarController {
         return IMOOCJSONResult.ok();
     }
 
+
+    @PostMapping("/del")
+    public IMOOCJSONResult del(@RequestParam String userId,
+                               @RequestParam String itemSpecId,
+                               HttpServletRequest request,
+                               HttpServletResponse response) {
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)) {
+            return IMOOCJSONResult.errorMsg("参数不能为空");
+        }
+
+        // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的数据
+        return IMOOCJSONResult.ok();
+    }
+
 }
