1d15a7383ab6fca7d7edf7b05c0abafa4829dc50
LivenChief <liven_zhan@chiefgroup.com.hk>
2025-01-23 10:28:27 +0800

    update iap orders table

--- foodie-pojo/src/main/java/com/imooc/pojo/IAPOrder.java
+++ foodie-pojo/src/main/java/com/imooc/pojo/IAPOrder.java
@@ -5,19 +5,25 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
-@TableName("iap_order")
+@TableName("iap_orders")
 public class IAPOrder {
     @TableId(type = IdType.AUTO)
     private Long id;
+    private String userId;
     private String transactionId;
-    private String receiptData;
+    private String originalTransactionId;
+    private String productId;
     private Integer status;
     private String environment;
-    private String productId;
     private Date purchaseDate;
+    private Date expiresDate;
+    private BigDecimal priceAmount;
+    private String currency;
+    private Integer quantity;
     private Date verifyDate;
     private Date createTime;
     private Date updateTime;
